cd102a14544fc6ee33b8f412950d2f8c2f405c8a
Md-Lei <dharenlei7@gmail.com>
2024-12-09 23:40:47 +0800

    Dynamic

--- A08/connect.php
+++ A08/connect.php
@@ -2,7 +2,7 @@
   $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "corememories";
+	$db = "corem";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
--- A08/index.php
+++ A08/index.php
@@ -1,73 +1,112 @@
-<!-- <?php 
-include ("connect.php");
+<?php
+include('connect.php');
 
-?> -->
+$query = "SELECT * FROM islandsofpersonality";
+$result = executeQuery($query);
+
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Core Memories</title>
+    <title>W3.CSS Template</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         body,
-        h1 {
-            font-family: "Raleway", Arial, sans-serif
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            font-family: "Lato", sans-serif
         }
 
-        h1 {
-            letter-spacing: 6px
+        .w3-bar,
+        h1,
+        button {
+            font-family: "Montserrat", sans-serif
         }
 
-        .w3-row-padding img {
-            margin-bottom: 12px
+        .fa-anchor,
+        .fa-coffee {
+            font-size: 200px
         }
     </style>
-
-    <link rel="icon" href="images/logo.png">
 </head>
 
 <body>
+    <header class="w3-container w3-center"
+        style="padding:128px 16px; background-image: url('images/bg.jpeg'); background-size: cover; background-position: center; color: white;">
+        <h1 class="w3-margin w3-jumbo">Core Memory</h1>
+        <p class="w3-xlarge">The foundation of being me.</p>
+        <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top"
+            onclick="window.location.href='../';">Home</button>
+    </header>
 
-    <div class="w3-content" style="max-width:1500px">
+    <?php
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $name = $row['name'];
+            $shortDescription = $row['shortDescription'];
+            $longDescription = $row['longDescription'];
+            $color = $row['color'];
+            $image = $row['image'];
+            $status = $row['status'];
+            ?>
 
-        <header class="w3-panel w3-center w3-opacity" style="padding:128px 16px">
-            <h1 class="w3-xlarge">Core Memories</h1>
-            <h1>Mike Dharen Luansing </h1>
-
-            <div class="w3-padding-32">
-                <div class="w3-bar w3-border">
-                    <a href="../" class="w3-bar-item w3-button"><strong>Home</strong></a>
-                    <a href="#" class="w3-bar-item w3-button w3-light-grey"><strong>Core Memories</strong></a>
-                    <!-- <a href="#" class="w3-bar-item w3-button">Contact</a>
-                    <a href="#" class="w3-bar-item w3-button w3-hide-small">Weddings</a> -->
+            <div class="w3-row-padding w3-padding-64 w3-container" style="background-color: <?php echo $color; ?>;">
+                <div class="w3-content">
+                    <div class="w3-twothird">
+                        <h1><?php echo $name; ?></h1>
+                        <p class="w3-text-grey py-2"><?php echo $shortDescription; ?></p>
+                        <p class="w3-text-grey py-2"><?php echo $longDescription; ?></p>
+                    </div>
+                    <div class="w3-third w3-center">
+                        <img src="images/<?php echo $image; ?>" alt="Island Image" class="w3-padding-4"
+                            style="max-width: 70%; height: auto;">
+                    </div>
+                    <div class="container d-flex justify-content-center align-items-center" style="height: 70px;">
+                        <button onclick="window.location.href='view.php'" class="btn btn-secondary"
+                            style="width: 100px;">Visit</button>
+                    </div>
                 </div>
             </div>
-        </header>
 
-        <div class="w3-row-padding w3-grayscale" style="margin-bottom:128px">
-            <div class="w3-half">
-                <img src="images/chess.jpeg" style="width:100%;  border-radius: 15px;">
-            </div>
+            <?php
+        }
+    } else {
+        echo "No records found.";
+    }
+    ?>
 
-            <div class="w3-half">
-                <img src="images/ml.jpg" style="width:100%; border-radius: 15px;">
-            </div>
-        </div>
-
+    <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+        <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
     </div>
 
-    <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-large">
-        <i class="fa fa-facebook-official w3-hover-opacity"></i>
-        <i class="fa fa-instagram w3-hover-opacity"></i>
-        <i class="fa fa-snapchat w3-hover-opacity"></i>
-        <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-        <i class="fa fa-twitter w3-hover-opacity"></i>
-        <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <footer class="w3-container w3-padding-64 w3-center w3-opacity">
+        <div class="w3-xlarge w3-padding-32">
+            <i class="fa fa-facebook-official w3-hover-opacity"></i>
+            <i class="fa fa-instagram w3-hover-opacity"></i>
+            <i class="fa fa-snapchat w3-hover-opacity"></i>
+            <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+            <i class="fa fa-twitter w3-hover-opacity"></i>
+            <i class="fa fa-linkedin w3-hover-opacity"></i>
+        </div>
+        <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
 </body>
 
